b411d69cfb18d7cb006d334421e54792af26856f
Ansh1234 <anshul.jain010@gmail.com>
2014-10-16 19:05:58 +0530

    added DoubleLinkedList.java for defining a node for doubly linked list

---
+++ src/List/DoubleLinkedListNode.java
@@ -0,0 +1,11 @@
+package List;
+
+/**
+ * Created by anshul on 16/10/14.
+ */
+public class DoubleLinkedListNode {
+
+    public Integer element;
+    public DoubleLinkedListNode next;
+    public DoubleLinkedListNode previous;
+}
